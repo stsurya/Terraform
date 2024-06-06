00e7ff9edea2f93ca5e42739221d6fee51e2c786
Surya Teja <chatrathisuryateja6@gmail.com>
2024-06-06 21:27:58 +0530

    first commit

---
+++ TfPrepCode/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "Suryarg-01"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "sauks01surya"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
