fd6b60fe7f6668e8097b240abd6d1747822b9ca2
Surya Teja <chatrathisuryateja6@gmail.com>
2024-06-07 22:07:58 +0530

    remved code

--- TfPrepCode/main.tf
+++ TfPrepCode/main.tf
@@ -17,11 +17,6 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "example02" {
-  name     = "Suryarg-02"
-  location = "West Europe"
-}
-
 resource "azurerm_storage_account" "example" {
   name                     = "sauks01surya"
   resource_group_name      = azurerm_resource_group.example.name
